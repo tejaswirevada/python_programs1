3a6aefa94da74451b25e00b5d59a05a07af7ac64
tejaswirevada <tejaswirevada2006@gmail.com>
2026-03-26 20:38:19 +0530

    Add files via upload

---
+++ Area of the rectangle.py
@@ -0,0 +1,5 @@
+#Area of the rectangle:
+Length=float(input("Enter length of the rectangle:"))
+Breadth=float(input("Enter breadth of the rectangle:"))
+Area=Length*Breadth
+print(f"Area of the rectangle is:{Area} sq.units")
