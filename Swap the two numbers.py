5ffbbe80a8b91fd57f94940b9190eb8830e01aab
tejaswirevada <tejaswirevada2006@gmail.com>
2026-03-27 18:44:41 +0530

    Add files via upload

---
+++ Swap the two numbers.py
@@ -0,0 +1,7 @@
+#Swapping the two values:
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+temp=a
+a=b
+b=temp
+print(f"After swapping a value is {a}\nAfter swapping b value is {b}")
