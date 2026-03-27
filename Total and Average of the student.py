c62c91d50b5cdc8c2d0a0f8163af3acf52c57ecb
tejaswirevada <tejaswirevada2006@gmail.com>
2026-03-27 19:02:12 +0530

    Add files via upload

---
+++ Total and Average of the student.py
@@ -0,0 +1,7 @@
+#Total and Average:
+M1=float(input("Enter marks in s1:"))
+M2=float(input("Enter marks in s2:"))
+M3=float(input("Enter marks in s3:"))
+total =M1+M2+M3
+average=total/3
+print(f"Total marks of the student is={total}\nAverage of the marks is={average}")
