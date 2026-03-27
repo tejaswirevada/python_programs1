7cac65e6f63e2f6e539f09c3c16778587f5fac82
tejaswirevada <tejaswirevada2006@gmail.com>
2026-03-27 19:01:06 +0530

    Add files via upload

---
+++ Functions_Maximum of two numbers.py
@@ -0,0 +1,9 @@
+def max():
+    a=int(input("Enter a number:"))
+    b=int(input("Enter another number:"))
+    if a>b:
+        print(f"{a} is bigger than {b}")
+    else:
+        print(f"{b} is bigger than {a}")
+max()
+    
