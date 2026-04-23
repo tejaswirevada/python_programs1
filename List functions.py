7228d566a6ec5e963f58a4e67368bf06b67b9d09
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:52:20 +0530

    Add files via upload

---
+++ List functions.py
@@ -0,0 +1,25 @@
+#List functions:
+'''len()
+min()
+max()
+sum()
+list()
+extend()'''
+List1=[2,3,4,5,10]
+List2=[0,5,9,15,1]
+print(len(List1))#5
+print(min(List1))#2
+print(max(List1))#10
+print(sum(List1))#24
+List1.extend([8,10])#Extending a list
+print(List1)
+b=[1,2,[3,4],5,6]#nested list
+print(b)
+print(len(b))
+
+
+
+
+#Matrix addition: hw
+
+
