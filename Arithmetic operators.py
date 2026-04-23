c97facc6f4b44a229539e6954f17f51495595aea
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:51:53 +0530

    Add files via upload

---
+++ Arithmetic operators.py
@@ -0,0 +1,9 @@
+#Arithmetic Operators:
+a=int(input("Enter a value:"))
+b=int(input("ENter b value:"))
+print("Sum is :",a+b)
+print("Subtraction is is:",a-b)
+print("Multiplication is:",a*b)
+print("Division is:",a/b)
+print("Floor division is:",a//b)
+print("Exponent is:",a**b)
