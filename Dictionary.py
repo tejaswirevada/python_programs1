23f4719f22a47723323d12b80028ed981ddb4da8
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:47:40 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,15 @@
+#Dictionary:
+'''collection of items
+Dict={}
+print(Dict)
+Dict[0]="Teju"
+Dict[1]="NagaLakshmi"
+Dict[2]="Mahesh"
+print(Dict)'''
+Dict={1:"Teju",2:827,3:100}
+Dict[3]=101
+print(Dict)
+
+
+
+
