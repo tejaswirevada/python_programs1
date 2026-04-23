6298eed976e87942f32cd29cd9aa65e85eaed646
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:44:02 +0530

    Add files via upload

---
+++ Builtin_Modules.py
@@ -0,0 +1,21 @@
+#Types of Modules:
+
+#math module:
+from math import sqrt
+print(sqrt(100))
+
+#random module:
+import random
+print(random.randint(1,10))
+
+#datetime module:
+import datetime
+print(datetime.datetime.now())
+
+#os module:
+'''import os
+print(
+
+#sys module:
+import sys
+print('''
