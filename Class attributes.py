e7976449d41a7283d79fd6c9f89ea2c8f4eeb80e
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:44:29 +0530

    Add files via upload

---
+++ Class attributes.py
@@ -0,0 +1,8 @@
+#Class attributes:
+class college:
+    collegename="aditya"
+    u="university"
+    def display(self):
+        print("College name is:",self.collegename+self.u)
+c=college()
+c.display()
