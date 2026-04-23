4b642e1dae87f66d4e268ceb7596781dc1bf6b6b
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:43:28 +0530

    Add files via upload

---
+++ Boolean expressions.py
@@ -0,0 +1,19 @@
+#Boolean expressions:
+#shows False for [],{},()
+a=20
+print(bool(a))
+
+b=10.4
+print(bool(b))
+
+c="python"
+print(bool(c))
+
+d=[]
+print(bool(d))
+
+e=()
+print(bool(e))
+
+f={}
+print(bool(f))
