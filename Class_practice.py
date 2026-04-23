ec9d3a4c4dc965afc634d4f7bc0baba058cdcb2d
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:45:58 +0530

    Add files via upload

---
+++ Class_practice.py
@@ -0,0 +1,9 @@
+class person:
+    attr1="Teju"
+    attr2=19
+    def display(self):
+        print("Name is",self.attr1)
+        print("Age is",self.attr2)
+p=person()
+print(p.attr1)
+p.display()
