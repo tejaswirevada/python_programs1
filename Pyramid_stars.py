56ec4ef60f90f847d718aacae3d54e73d6066a47
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:58:51 +0530

    Add files via upload

---
+++ Pyramid_stars.py
@@ -0,0 +1,7 @@
+n=int(input("Enter number of rows:"))
+for i in range(1,n+1):
+    for spacce in range(n-i):
+        print(" ",end="")
+    for star in range(2*i-1):
+        print("*",end="")
+    print()
