8590d90a1897d3680d25c216e7c62c42b0ec6985
tejaswirevada <tejaswirevada2006@gmail.com>
2026-04-23 11:52:44 +0530

    Add files via upload

---
+++ List methods.py
@@ -0,0 +1,18 @@
+#List methods:
+colour_list=["red","blue","purple","black"]
+'''colour_list.append("green")
+print(colour_list)
+colour_list.insert(1,"pink")
+print(colour_list)
+colour_list.remove("black")
+print(colour_list)
+colour_list.pop(2)
+print(colour_list)
+colour_list.sort()
+print(colour_list)
+colour_list.reverse()
+print(colour_list)
+colour_list.copy(colour_list)
+print(colour_list)'''
+print(colour_list.index("blue"))
+print(colour_list.count("red"))
